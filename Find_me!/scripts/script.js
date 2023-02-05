e345f2350cad33926509a9d63eef9a97fa212ee0
Philip Mellbin <Gamer.mellbin@gmail.com>
2023-02-05 21:33:43 +0100

    changed score registration in on Click function

--- Find_me!/scripts/script.js
+++ Find_me!/scripts/script.js
@@ -43,7 +43,8 @@ var score = document.getElementById("score")
 
 var collors = ["blank", "red", "green", "blue"];
 var curentcollor = "blank"
-var points = 0
+var points = 0;
+var check = 0;
 var iris = document.getElementById(curentcollor) //needs to be id in order to change. So just define id as the only non game one(blank)
 
 /////////////////////////////////////////////////////////////////////////////////////////////(Preset conditions)
@@ -90,21 +91,19 @@ async function page(eye)//Switch page
   game.style.display = "block";
 
 }
-function Click(button, i, score, points) //Click function(Can't "onclick" directly(https://www.youtube.com/watch?v=aZbgE3yhC2o&t=342s))
+function Click(button, i) //Click function(Can't "onclick" directly(https://www.youtube.com/watch?v=aZbgE3yhC2o&t=342s))
   {
     button[i].onclick = function()
     {
       console.log(button[i].id);
       if(button[i].id == curentcollor) //if the buttons id is equal to the curent color
       {
-        points++
-        score.innerHTML = points;
-        console.log(score.innerHTML)
+       check = 1
       }
       curentcollor = switchcollor(collors, iris, curentcollor); //switch collors and log the corect one
       console.log("corect collor: " + curentcollor);
     }
-    return points //return points for counting
+    return check //return points for counting
   }
 /////////////////////////////////////////////////////////////////////////////////////////////////(On clicks)
 
@@ -126,9 +125,10 @@ for(i = 0; i < start.length; i++) //For every button with the class start: add a
 };
 for(i = 0; i < button.length; i++) //Add on click function for 
 { //Aparently can't put button[i].id directly in if section.
-  points = Click(button, i, score, points);
+  check = Click(button, i);
 }
-
+points = points + check;
+score.innerHTML = points;
 });
 
 
